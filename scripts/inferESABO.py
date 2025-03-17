7aa317040d68f4bb326436b97e4cce7b700b991a
Viktor Dinkel <viktor.dinkel@awi.de>
2025-03-17 14:44:57 +0100

    updated ESABO script

--- scripts/inferESABO.py
+++ scripts/inferESABO.py
@@ -9,14 +9,16 @@ import numpy as np
 import pandas as pd
 
 def msum(A):
+    # returns the sum of a matrix
     return int(np.sum(np.sum(A)))
 
 def binNorm(matrix):
+    # normalizes the matrix to contain only 0 and 1 values (presence/absence)
     retM = matrix.where(matrix == 0, 1)
     return retM
 
 def ESABO(x1, x2):
-    # ESABO test
+    # compute the ESABO score between two input vectors
     xij = np.logical_and(x1,x2).astype(int)
     p1 = np.sum(xij) / len(xij)
     
@@ -32,19 +34,19 @@ def ESABO(x1, x2):
     return H_real, z_score
 
 def corrESABOM(matrix, thresh):
+    # compute the ESABO matrix given input abundance matrix and z-score threshold
     # columns = taxa/features; rows = samples/observations
     
     ESABO_A = np.zeros((matrix.shape[1],matrix.shape[1])) # matrix[y][x]
-    #ESABO_p = np.zeros((matrix.shape[0],matrix.shape[0])) # matrix[y][x]
     
     for i in range(0,matrix.shape[1]):
         for j in range(0,matrix.shape[1]):
             if i==j:
-                ESABO_A[i][j] = 0
+                ESABO_A[i][j] = 0 # diagonal is 0
             else:
                 arr1 = matrix[matrix.columns[i]].values # abundance vector of taxon i
                 arr2 = matrix[matrix.columns[j]].values # abundance vector of taxon j
-                if (0 in arr1 and 1 in arr1 and 0 in arr2 and 1 in arr2):
+                if (0 in arr1 and 1 in arr1 and 0 in arr2 and 1 in arr2): # there must be presence/absence variation in the vectors
                     H_real, z_score = ESABO(arr1, arr2)
                     if np.isnan(z_score):
                         import pdb; pdb.set_trace()
@@ -52,28 +54,30 @@ def corrESABOM(matrix, thresh):
                 else:
                     #print ("not compatible with esabo")
                     ESABO_A[i][j] = 0
-                
+    
+    # set the esabo indeces and columns to the column names of input matrix
     ESABO_A = pd.DataFrame(ESABO_A)
     ESABO_A.columns = matrix.columns
     ESABO_A.index = matrix.columns
     
-    ESABO_A = ESABO_A.where(abs(ESABO_A) > thresh, 0) # z score must be > 2
-    #ESABO_A[ESABO_A != 0] = 1
+    # apply threshold to ESABO adjacency matrix, set values below threshold to 0 
+    ESABO_A = ESABO_A.where(abs(ESABO_A) > thresh, 0) # z score must be larger than given threshold
     
-    return ESABO_A#.astype(int)
+    return ESABO_A
 
 def fillDiagonal(dataFrame):
     for index in dataFrame.columns:
         dataFrame.at[index,index] = 0
     return dataFrame
 
-s_M = pd.read_csv("C:/Users/vdinkel/Desktop/Manuscript/input/F_KL-77.csv", delimiter=",", header=0, index_col=0)
+workdir ="/home/viktor/project_migration/MarineFoodWeb-sedaDNA/"
+s_M = pd.read_csv(workdir+"input/F_KL-77.csv", delimiter=",", header=0, index_col=0)
 
-#ESABO
+#compute ESABO adjacency matrix and save into output folder
 s_M_bin = binNorm(s_M)
 s_esaboM = corrESABOM(s_M_bin, 1.3)
 s_esaboM = fillDiagonal(s_esaboM)
 s_absEsabo = abs(s_esaboM.round().astype(int))
 s_absEsabo = s_absEsabo.where(s_absEsabo == 0, 1)
 print(msum(s_absEsabo))
-pd.DataFrame.to_csv(s_absEsabo, "C:/Users/vdinkel/Desktop/Manuscript/output/KL-77_esabo.csv", sep=";")
+pd.DataFrame.to_csv(s_absEsabo, workdir+"output/KL-77_esabo.csv", sep=";")
